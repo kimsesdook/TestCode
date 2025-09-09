0c7775467b38564c524eca67ca2ca1ab02de2c2c
kimsesdook <kimsesdook@gmail.com>
2025-09-09 17:19:01 +0900

    add  " add commit"

--- Test/src/com/kh/test01/Run.java
+++ Test/src/com/kh/test01/Run.java
@@ -5,6 +5,7 @@ public class Run {
 	public static void main(String[] args) {
 		System.out.println("안녕하세요 김정훈 입니다.");
         System.out.println("안녕하세요 김정훈 아닙니다.");
+        System.out.println("안녕 못합니다.");
 	}
 
 }
