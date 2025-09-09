a5090c32fd12fd4521bb781bfaf39944d253b28c
kimsesdook <kimsesdook@gmail.com>
2025-09-09 17:20:55 +0900

    no message

--- Test/src/com/kh/test01/Run.java
+++ Test/src/com/kh/test01/Run.java
@@ -6,6 +6,7 @@ public class Run {
 		System.out.println("안녕하세요 김정훈 입니다.");
         System.out.println("안녕하세요 김정훈 아닙니다.");
         System.out.println("안녕 못합니다.");
+        System.out.println("삭제 왜 하냐 임마");
 	}
 
 }
