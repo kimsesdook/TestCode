d9538210e19d83907b06c49a09ac9756309b1950
kimsesdook <kimsesdook@gmail.com>
2025-09-09 17:33:52 +0900

    .

--- Test/src/com/kh/test01/Run.java
+++ Test/src/com/kh/test01/Run.java
@@ -3,14 +3,9 @@ package com.kh.test01;
 public class Run {
 
 	public static void main(String[] args) {
-		System.out.println("안녕하세요 김정훈 입니다.");
-<<<<<<< HEAD
         System.out.println("안녕하세요 김정훈 아닙니다.");
         System.out.println("안녕 못합니다.");
         System.out.println("삭제 왜 하냐 임마");
-=======
-        
->>>>>>> 7e85643df681352697751d939a9a61bef8206775
 	}
 
 }
